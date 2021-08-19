e2b58b3ba23ed3c8c61e388379ecedfd3d713f79
bwbarbosa <bruno.barbosa45@fatec.sp.gov.br>
2021-08-19 23:27:25 +0000

    (19/08) Término do bubble sorte r início do selection sort

---
+++ 04_selection_sort.py
@@ -0,0 +1,9 @@
+# Algortimo de ordenação Selection Sort
+
+# Isola (seleciona) o primeiro elemento da lista e, em seguida,
+# encontra o menor valor no restante da lista. Se o valor encontrado
+# for menor que o valor selecionado, efetua a troca.
+# Em seguida, isola o segundo número da lista, buscando pelo menor
+# valor das posições sebsequentes. Faz a troca entre os dois valores, 
+# se necessário. Continua o processo, até isolar o penúltimo elemento 
+# da lista.
